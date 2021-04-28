6a70228b0500427728bfb1dccfb380f66db8a1b1
Danielle Barnas <danielle.barnas@csun.edu>
2021-04-28 11:38:48 -0700

    if ct.serial != FALSE and default is FALSE

--- R/CT_roundup.R
+++ R/CT_roundup.R
@@ -12,7 +12,7 @@
 #' @param tf_recursive Logical parameter indicating whether to search within folders at the data.path. Default = FALSE
 #' @return For every imported CT data file, one tidied file with temperature-compensated conductance is exported and returned
 #' @export
-CT_roundup<-function(data.path, output.path, ct.serial = NULL, tf_write, tf_recursive = FALSE){
+CT_roundup<-function(data.path, output.path, ct.serial = FALSE, tf_write, tf_recursive = FALSE){
 
   # Create a list of all files within the directory folder
   file.names.Cal<-basename(list.files(data.path, pattern = c("csv$", recursive = tf_recursive))) #list all csv file names in the folder and subfolders
@@ -75,7 +75,7 @@ CT_roundup<-function(data.path, output.path, ct.serial = NULL, tf_write, tf_recu
     }
   }
 
-  if(exists(as.character('ct.serial')|as.numeric('ct.serial'))) {
+  if(ct.serial!=FALSE) {
     pattern <- grep(x = full_df$List.ID, pattern = ct.serial, value = TRUE)
     full_df <- full_df %>%
       dplyr::filter(List.ID == pattern[2])
