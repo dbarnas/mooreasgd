40617fe499e2db2ef313840bde4cf7593f618445
Danielle Barnas <danielle.barnas@csun.edu>
2021-05-07 03:38:55 -0700

    replace TempInSitu with temp parameter

--- R/CT_one_cal.R
+++ R/CT_one_cal.R
@@ -30,7 +30,7 @@ CT_one_cal<-function(data, date, cal.ref, EC, temp, Abs_pressure = 10, startCal,
   # use mean temperature of calibrations with PSS-78 and gsw package
   # Get EC of conductivity standard at logged temperature to calibrate logged EC readings
   cal.sp<-gsw_SP_from_C(C = cal.ref*0.001, t = 25, p = Abs_pressure)
-  cal.ref<-1000*gsw_C_from_SP(SP = cal.sp, t = TempInSitu, p = Abs_pressure)
+  cal.ref<-1000*gsw_C_from_SP(SP = cal.sp, t = {{temp}}, p = Abs_pressure)
 
   # Logger data in pre-deployment calibration
   mean.ec<-data%>%
