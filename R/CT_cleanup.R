c67d433d187bb5361f30bca3825806a9d5358f2a
dbarnas <danielle.barnas@csun.edu>
2021-09-02 11:48:12 -0700

    removed single cleanup fxn, renamed roundup fxn as cleanup, updated path parameter name

--- R/CT_cleanup.R
+++ R/CT_cleanup.R
@@ -1,58 +1,65 @@
-#' Clean up raw conductivity-temperature, CT, data file from the HOBO CT Logger
+#' Import, clean up, and export multiple raw conductivity-temperature, CT, data files from the HOBO CT Logger
 #'
-#' This function loads a raw CT file. It searches a directory for the correct logger's serial number,
-#' plucks out the correct file or files and stacks all related files into one loaded csv.
-#' It removes the first row of each relevant file containing logger information, then retains column headers.
+#' This function loads all raw .csv files within the data.path folder.
+#' It removes the first row of each file containing logger information, then retains column headers.
 #' Unnecessary columns are removed, and the remaining columns are renamed as needed for simplicity and consistency.
 #' Any rows containing NA's are dropped.
 #'
 #' @param data.path Path to the input files
 #' @param output.path Path for the output files
-#' @param ct.serial Logger serial number used in naming the input file
-#' @param write_tf Logical parameter indicating whether to save output files in an output folder. Default = FALSE.
-#' @param recursive_tf Logical parameter indicating whether to search within folders at the file path. Default = FALSE
-#' @return A cleaned dataframe of CT logger data
+#' @param path.pattern Character string or object referencing a character string that identifies CT logger data within the specified file path
+#' @param tf.write Logical parameter indicating whether to save output files in an output folder. Default = FALSE.
+#' @param tf.recursive Logical parameter indicating whether to search within folders at the data.path. Default = FALSE
+#' @return For every imported CT data file, one tidied file with temperature-compensated conductance is exported and returned
 #' @export
-CT_cleanup <- function(data.path, ct.serial, output.path, write_tf = FALSE, recursive_tf = FALSE) {
+CT_cleanup<-function(data.path, output.path, path.pattern, tf.write = FALSE, tf.recursive = FALSE){
 
-  file.names.Cal<-ba
-  sename(list.files(data.path, pattern = c(ct.serial,"csv$", recursive = recursive_tf))) #list all csv file names in the folder and subfolders
+  # Create a list of all files within the directory folder
+  file.names.Cal<-basename(list.files(data.path, pattern = path.pattern, recursive = tf_recursive)) #list all csv file names in the folder and subfolders
 
-  condCal <- file.names.Cal %>%
-    purrr::map_dfr(~ readr::read_csv(file.path(data.path, .), skip=1, col_names=T)) # read all csv files at the file path, skipping 1 line of metadata
+  # Create an empty dataframe to store all subsequent tidied df's into
+  full_df <- tibble::tibble(
+    date = as_datetime(NA),
+    List.ID = as.character(),
+    TempInSitu = as.numeric(),
+    E_Conductivity = as.numeric())
 
-  condCal<-condCal %>%
-    dplyr::select(contains('Date'), contains(ct.serial), contains("High Range"), contains("Temp")) %>% # Filter specified probe by Serial number
-    dplyr::mutate(LoggerID=paste0("CT_",ct.serial)) %>% # add column for CT serial number
-    dplyr::rename(date=contains("Date"),
-                  TempInSitu=contains("Temp"),
-                  E_Conductivity=contains("High Range")) %>%
-    tidyr::drop_na() %>%
-    dplyr::mutate(date = lubridate::mdy_hms(date))
+  # For each listed file:
+  ## Load the dataframe,
+  ## Tidy the data,
+  ## Create a column for temperature-compensated specific conductance, and
+  ## Export the new file to an output folder
+  for(i in 1:length(file.names.Cal)) {
+    Data_ID<-file.names.Cal[[i]]
 
-  # condCal$date <- condCal$date %>%
-  #   readr::parse_datetime(format = "%m/%d/%y %H:%M:%S %p", # Convert 'date' to date and time vector type
-  #                         na = character(),
-  #                         locale = default_locale(),
-  #                         trim_ws = TRUE)
+    file.names<-basename(list.files(data.path, pattern = c(Data_ID, "csv$"), recursive = tf_recursive)) #list all csv file names in the folder and subfolders
+
+    condCal <- file.names %>%
+      purrr::map_dfr(~ readr::read_csv(file.path(data.path, .), skip=1, col_names=T)) # read all csv files at the file path, skipping 1 line of metadata and bind together
+
+    condCal<-condCal %>%
+      dplyr::select(contains('Date'), contains("High Range"), contains("Temp")) %>%
+      dplyr::mutate(List.ID=Data_ID) %>%  # add column for file ID
+      dplyr::rename(date=contains("Date"),
+                    TempInSitu=contains("Temp"),
+                    E_Conductivity=contains("High Range")) %>%
+      tidyr::drop_na() %>%
+      tidyr::separate(col = 'List.ID', into = c('List.ID',NA), sep = ".csv", remove = T) %>%  # remove the '.csv'
+      dplyr::mutate(date = lubridate::mdy_hms(date))
 
 
+    full_df <- full_df %>%
+      dplyr::full_join(condCal) # save your dataframes into a larger df
 
-  ############################################################
-  ### Nonlinear Temperature Compensation
-  ############################################################
+    # conditional write.csv at output path
+    if(tf_write == TRUE) {
+      write.csv(condCal, paste0(output.path,'/',Data_ID,'_tidy.csv'))
+    }
+   }
 
-  # # https://www.aqion.de/site/112
-  # condCal<-condCal %>%
-  #   mutate(A = (1.37023 * (TempInSitu - 20)) + 8.36 * (10^(-4) * ((TempInSitu - 20)^2))) %>%
-  #   mutate(B = 109 + TempInSitu) %>%
-  #   mutate(Sp_Conductance = 0.889 * (10^(A/B)) * E_Conductivity) %>%
-  #   dplyr::select(-c(A,B)) # remove intermediate columns
 
-  # conditional write.csv at output path
-  if(write_tf == TRUE) {
-    write.csv(condCal, paste0(output.path,'/CT_',ct.serial,'_tidy.csv'))
-  }
+  full_df <- full_df %>%
+    dplyr::rename(LoggerID = List.ID)
 
-  return(condCal) # return dataframe
+  return(full_df) # return full dataframe
 }
--- R/WL_cleanup.R
+++ R/WL_cleanup.R
@@ -1,46 +1,76 @@
-#' Clean up raw depth data file from the HOBO water level Logger
+#' Import, clean up, and export multiple raw conductivity-temperature, CT, data files from the HOBO CT Logger
 #'
-#' This function loads a raw water level file. It searches a directory for the correct logger's serial number,
-#' plucks out the correct file or files and stacks all related files into one loaded csv.
-#' It removes the first row of each relevant file containing logger information, then retains column headers.
+#' This function loads all raw .csv files within the data.path folder.
+#' It removes the first row of each file containing logger information, then retains column headers.
 #' Unnecessary columns are removed, and the remaining columns are renamed as needed for simplicity and consistency.
 #' Any rows containing NA's are dropped.
 #'
 #' @param data.path Path to the input files
 #' @param output.path Path for the output files
-#' @param wl.serial Logger serial number used in naming the input file
-#' @param tf_write Logical parameter indicating whether to save output files in an output folder. Default = FALSE.
-#' @param tf_recursive Logical parameter indicating whether to search within folders at the file path. Default = FALSE
-#' @return A cleaned dataframe of pH logger data
+#' @param wl.serial Character string containing serial ID for a particular probe.  Used to filter from full dataset before returning final dataframe
+#' @param tf_write Logical parameter indicating whether to save output files in an output folder. No default.
+#' @param tf_recursive Logical parameter indicating whether to search within folders at the data.path. Default = FALSE
+#' @return For every imported CT data file, one tidied file with temperature-compensated conductance is exported and returned
 #' @export
-WL_cleanup <- function(data.path, wl.serial, output.path, tf_write = FALSE, tf_recursive = FALSE) {
+WL_cleanup<-function(data.path, output.path, wl.serial = FALSE, tf_write = FALSE, tf_recursive = FALSE){
 
-  file.names.wl<-basename(list.files(data.path, pattern = c(wl.serial,"csv$"), recursive = tf_recursive)) #list all csv file names in the folder and subfolders
+  # Create a list of all files within the directory folder
+  file.names.wl<-basename(list.files(data.path, pattern = c("Pressure","csv$"), recursive = tf_recursive)) #list all csv file names in the folder and subfolders
 
-  depthLog <- file.names.wl %>%
-    purrr::map_dfr(~ readr::read_csv(file.path(data.path, .), skip=1, col_names=T)) # read all csv files at the file path, skipping 1 line of metadata
+  # Create an empty dataframe to store all subsequent tidied df's into
+  full_df <- tibble::tibble(
+    date = as_datetime(NA),
+    List.ID = as.character(),
+    AbsPressure=as.numeric(),
+    Depth=as.numeric(),
+    TempInSitu = as.numeric())
 
-  depthLog<-depthLog %>%
-    dplyr::select(contains('Date'), contains(wl.serial), contains("Temp"),contains("Abs Pres"), contains("Water Level")) %>% # Filter specified probe by Serial number
-    dplyr::mutate(LoggerID=paste0("WL_",wl.serial)) %>% # add column for CT serial number
-    dplyr::rename(date=contains("Date"),
-                  TempInSitu=contains("Temp"),
-                  AbsPressure=contains("Abs Pres"),
-                  Depth=contains("Water Level")) %>%
-    tidyr::drop_na() %>%
-    dplyr::mutate(date = lubridate::mdy_hms(date),
-                  Pres_dbar = AbsPressure / 10) # Absolute Pressure recorded in kPa. 1 kPa = 10dbar
+  # For each listed file:
+  ## Load the dataframe,
+  ## Tidy the data,
+  ## Create a column for temperature-compensated specific conductance, and
+  ## Export the new file to an output folder
+  for(i in 1:length(file.names.wl)) {
+    Data_ID<-file.names.wl[[i]]
 
-  # depthLog$date <- depthLog$date %>%
-  #   readr::parse_datetime(format = "%m/%d/%y %H:%M:%S %p", # Convert 'date' to date and time vector type
-  #                         na = character(),
-  #                         locale = default_locale(),
-  #                         trim_ws = TRUE)
+    file.names<-basename(list.files(data.path, pattern = c(Data_ID, "csv$"), recursive = tf_recursive)) #list all csv file names in the folder and subfolders
 
-  # conditional write.csv at output path
-  if(tf_write == TRUE) {
-    write.csv(depthLog, paste0(output.path,'/Pressure_',wl.serial,'_tidy.csv'))
+    depthLog <- file.names %>%
+      purrr::map_dfr(~ readr::read_csv(file.path(data.path, .), skip=1, col_names=T)) # read all csv files at the file path, skipping 1 line of metadata and bind together
+
+    depthLog<-depthLog %>%
+      dplyr::select(contains('Date'), contains("Temp"),contains("Abs Pres"), contains("Water Level")) %>%
+      dplyr::mutate(List.ID=Data_ID) %>%  # add column for file ID
+      dplyr::rename(date=contains("Date"),
+                    TempInSitu=contains("Temp"),
+                    AbsPressure=contains("Abs Pres"),
+                    Depth=contains("Water Level")) %>%
+      tidyr::drop_na() %>%
+      tidyr::separate(col = 'List.ID', into = c('List.ID',NA), sep = ".csv", remove = T) %>%  # remove the '.csv'
+      dplyr::mutate(date = lubridate::mdy_hms(date))
+
+    # Format date and time
+    # condCal$date <- condCal$date %>%
+    #   readr::parse_datetime(format = "%m/%d/%y %H:%M:%S %p", # Convert 'date' to date and time vector type
+    #                         na = character(),
+    #                         locale = default_locale(),
+    #                         trim_ws = TRUE)
+
+
+
+    if(tf_write != FALSE) {
+      write.csv(depthLog, paste0(output.path,'/',Data_ID,'_WaterLevel.csv'))
+    }
   }
 
-  return(depthLog)
+  if(wl.serial!=FALSE) {
+    pattern <- grep(x = full_df$List.ID, pattern = wl.serial, value = TRUE)
+    full_df <- full_df %>%
+      dplyr::filter(List.ID == pattern[2])
+  }
+
+  full_df <- full_df %>%
+    rename(Serial = List.ID)
+
+  return(full_df) # return a list of dataframes
 }
