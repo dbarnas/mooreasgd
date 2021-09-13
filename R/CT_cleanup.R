69c26378a7269f707fdb56ce5f0362ea89083080
dbarnas <danielle.barnas@csun.edu>
2021-09-12 18:37:17 -0700

    add processing option for hoboware-calibrated csv

--- R/CT_cleanup.R
+++ R/CT_cleanup.R
@@ -10,10 +10,12 @@
 #' @param path.pattern Character string or object referencing a character string that identifies CT logger data within the specified file path
 #' @param tf.write Logical parameter indicating whether to save output files in an output folder. Default = FALSE.
 #' @param tf.recursive Logical parameter indicating whether to search within folders at the data.path. Default = FALSE
+#' @param hobo.cal Logical parameter indicating whether the .hobo file has been pre-calibrated using HOBOware software and exported as a processed .csv. Default = FALSE
 #' @return For every imported CT data file, one tidied file with temperature-compensated conductance is exported and returned
 #' @export
-CT_cleanup<-function(data.path, output.path, path.pattern, tf.write = FALSE, tf.recursive = FALSE){
+CT_cleanup<-function(data.path, output.path, path.pattern, tf.write = FALSE, tf.recursive = FALSE, hobo.cal = FALSE){
 
+  if(hobo.cal == FALSE){
   # Create a list of all files within the directory folder
   file.names.Cal<-basename(list.files(data.path, pattern = path.pattern, recursive = tf.recursive)) #list all csv file names in the folder and subfolders
 
@@ -60,6 +62,56 @@ CT_cleanup<-function(data.path, output.path, path.pattern, tf.write = FALSE, tf.
 
   full_df <- full_df %>%
     dplyr::rename(LoggerID = List.ID)
+  } else { # end of if(hobo.cal == FALSE)
+    # Create a list of all files within the directory folder
+    file.names.Cal<-basename(list.files(data.path, pattern = path.pattern, recursive = tf.recursive)) #list all csv file names in the folder and subfolders
+
+    # Create an empty dataframe to store all subsequent tidied df's into
+    full_df <- tibble::tibble(
+      date = as_datetime(NA),
+      List.ID = as.character(),
+      TempInSitu = as.numeric(),
+      E_Conductivity = as.numeric(),
+      Salinity_psu = as.numeric())
+
+    # For each listed file:
+    ## Load the dataframe,
+    ## Tidy the data,
+    ## Create a column for temperature-compensated specific conductance, and
+    ## Export the new file to an output folder
+    for(i in 1:length(file.names.Cal)) {
+      Data_ID<-file.names.Cal[[i]]
+
+      file.names<-basename(list.files(data.path, pattern = c(Data_ID, "csv$"), recursive = tf.recursive)) #list all csv file names in the folder and subfolders
+
+      condCal <- file.names %>%
+        purrr::map_dfr(~ readr::read_csv(file.path(data.path, .), skip=1, col_names=T)) # read all csv files at the file path, skipping 1 line of metadata and bind together
+
+      condCal<-condCal %>%
+        dplyr::select(contains('Date'), contains("High Range"), contains("Temp"), contains("Salinity")) %>%
+        dplyr::mutate(List.ID=Data_ID) %>%  # add column for file ID
+        dplyr::rename(date=contains("Date"),
+                      TempInSitu=contains("Temp"),
+                      E_Conductivity=contains("High Range"),
+                      Salinity_psu=contains("Salinity")) %>%
+        tidyr::drop_na() %>%
+        tidyr::separate(col = 'List.ID', into = c('List.ID',NA), sep = ".csv", remove = T) %>%  # remove the '.csv'
+        dplyr::mutate(date = lubridate::mdy_hms(date))
+
+
+      full_df <- full_df %>%
+        dplyr::full_join(condCal) # save your dataframes into a larger df
+
+      # conditional write.csv at output path
+      if(tf.write == TRUE) {
+        write.csv(condCal, paste0(output.path,'/',Data_ID,'_tidy.csv'))
+      }
+    }
+
+
+    full_df <- full_df %>%
+      dplyr::rename(LoggerID = List.ID)
+  } # end of if(hobo.csv == TRUE)
 
   return(full_df) # return full dataframe
 }
